37d493ebc82d4c1c134f4c0ee5a8dc28f2534cc4
Ohgyuchan <gyu3637@gmail.com>
2021-04-06 22:09:16 +0900

    Add midterm project(MDC 101-102 code completed)

---
+++ midterm/lib/home.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
+
+class HomePage extends StatelessWidget {
+  // TODO: Make a collection of cards (102)
+  // Replace this entire method
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.headline6,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.subtitle2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.fromLTRB(30, 100, 100, 20),
+              child: Text('Pages',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.normal)),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        title: Text('SHRINE'),
+        // TODO: Add trailing buttons (102)
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          // TODO: Build a grid of cards (102)
+          children: _buildGridCards(context) // Replace
+          ),
+      // TODO: Set resizeToAvoidBottomInset (101)
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
---
+++ midterm/lib/login.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE'),
+              ],
+            ),
+            SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+                FlatButton(
+                  child: Text('CANCEL', style: TextStyle(color: Colors.black),),
+                  onPressed: () {
+                    // TODO: Clear the text fields (101)
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                FlatButton(
+                  child: Text('Sign Up', style: TextStyle(color: Colors.black),),
+                  onPressed: () {
+                    // TODO: Clear the text fields (101)
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupPage()),
+                    );
+                  },
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                SizedBox(
+                  width: 100.0,
+                  child: RaisedButton(
+                      child: Text('NEXT'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }
+                  ),
+                ),
+              ],
+            ),
+            // TODO: Wrap Username with AccentColorOverride (103)
+            // TODO: Remove filled: true values (103)
+            // TODO: Wrap Password with AccentColorOverride (103)
+            // TODO: Add button bar (101)
+          ],
+        ),
+      ),
+    );
+  }
+}
+class SignupPage extends StatelessWidget {
+  final _username = GlobalKey<FormState>();
+  final _password = GlobalKey<FormState>();
+  final _confirm = GlobalKey<FormState>();
+  final _email = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+            SizedBox(height: 80.0),
+            Form(
+              key: _username,
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Username';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Username',
+                ),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            Form(
+              key: _password,
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Password';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+            ),
+              SizedBox(height: 12.0),
+              Form(
+                key: _confirm,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter Confirm Password';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Confirm Password',
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              SizedBox(height: 12.0),
+              Form(
+                key: _email,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter Email Address';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Email Address',
+                  ),
+                ),
+              ),
+              ButtonBar(
+                children: [
+                  SizedBox(
+                    width: 100.0,
+                    child: RaisedButton(
+                      child: Text('SIGN UP'),
+                      onPressed: () {
+                        var _valid1 = _username.currentState.validate();
+                        var _valid2 = _password.currentState.validate();
+                        var _valid3 = _confirm.currentState.validate();
+                        var _valid4 = _email.currentState.validate();
+                        if(_valid1 && _valid2 && _valid3 && _valid4)
+                          Navigator.pop(context);
+                      }
+                    ),
+                  ),
+                ],
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
