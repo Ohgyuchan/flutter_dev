fc52bfb3b1cbee02143e0cd587daf8ea74c83f4f
Ohgyuchan <gyu3637@gmail.com>
2021-05-19 20:23:28 +0900

    modified login.dart

--- finalterm/lib/login.dart
+++ finalterm/lib/login.dart
@@ -50,6 +50,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
 class AnonymousSignInButton extends StatefulWidget {
   @override
   _AnonymousSignInButtonState createState() => _AnonymousSignInButtonState();
